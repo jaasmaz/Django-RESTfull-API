ad13bd9b1f4a19c22c71a4b7e60b85796aea6660
jaasmaz <jaasmaz@gmail.com>
2022-01-17 20:57:56 -0800

    Using Model Serializer

--- store/serializers.py
+++ store/serializers.py
@@ -4,20 +4,18 @@ from store.models import Collection, Product
 from decimal import Decimal
 
 class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name= 'collection-detail',
-    )
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price', 'price_with_tax', 'collection', 'updated_on']
+
     updated_on = serializers.DateTimeField(source='last_update')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
     def calculate_tax(self, product: Product):
         return product.price * Decimal(1.1)
