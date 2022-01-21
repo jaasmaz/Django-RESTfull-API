be67ae7c1e228c2f4865144879522be4190de645
jaasmaz <jaasmaz@yahoo.com>
2022-01-20 23:57:53 -0800

    Pagination

--- store/views.py
+++ store/views.py
@@ -3,10 +3,13 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
-from store.filters import ProductFilter
+
+from pagination import DefaultPagination
 from .models import Collection, OrderItem, Product, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .filters import ProductFilter
 
 # Generic API View Class
 class ProductViewSet(ModelViewSet):
@@ -16,6 +19,7 @@ class ProductViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     # filterset_fields = ['inventory']
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title','description']
     ordering_fields = ['price', 'last_update']
 
