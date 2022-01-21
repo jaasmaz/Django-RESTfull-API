24c0ae948440c56d80f906d97b2a8fba339ef6bd
jaasmaz <jaasmaz@yahoo.com>
2022-01-20 23:06:26 -0800

    Nested Routers

--- store/urls.py
+++ store/urls.py
@@ -1,16 +1,17 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
-# router.register('products', views.ProductViewSet)
 
-
+products_router = routers.NestedDefaultRouter(router, 'products', lookup= 'product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
 # URLConf
 urlpatterns = [
-path('', include(router.urls))
+path('', include(router.urls)),
+path('', include(products_router.urls)),
 ]
  
