c9b4992c737f5a4554ae3368f24e16eef0a0abb7
jaasmaz <jaasmaz@yahoo.com>
2022-01-20 22:35:22 -0800

    ViewSets and Routers

--- store/views.py
+++ store/views.py
@@ -1,47 +1,29 @@
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Collection, Product
+from rest_framework.viewsets import ModelViewSet
+from .models import Collection, OrderItem, Product
 from .serializers import ProductSerializer, CollectionSerializer
 
 # Generic API View Class
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {'request': self.request}
 
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-    def delete(self, request,pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product Cannot be deleted its associated with order item'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status.HTTP_204_NO_CONTENT)
+        return super().destroy(request,*args,**kwargs)
 
-
-class CoillectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(product_count = Count('products')).all()
     serializer_class = CollectionSerializer
 
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(products_count = Count('products'))
-    serializer_class = CollectionSerializer
-
-    def delete(self, request,pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response({'error': 'collection Cannot be deleted its associated with a product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status.HTTP_204_NO_CONTENT)
-
+        return super().destroy(request,*args,**kwargs)
